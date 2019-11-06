1f038b8194570c5b2ea43c299dfd4450f4ef21e5
Robert Banaszak <robert.banaszak1997@gmail.com>
2019-11-06 19:40:57 +0100

    test cd

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => res.json({ data: "Polifood" }));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, '0.0.0.0',() => console.log(`Example app listening on port ${port}!`));
