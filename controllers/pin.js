70cd4f0e4112f4e16ae670dbaf0b91d53ab5269e
Jan Sobanski <sobanski.jan2@gmail.com>
2019-12-05 05:08:08 +0100

    add data validation and delete support

--- controllers/pin.js
+++ controllers/pin.js
@@ -1,95 +1,172 @@
-const { error, success } = require('./index')
-const db = require('../models')
+const { error, success } = require("./index");
+const db = require("../models");
 
 exports.findPins = (req, res) => {
-    const { location, distance, categories } = req.body
-    let query = {
-        location: {
-            $near: {
-                $maxDistance: distance,
-                $geometry: {
-                    type: "Point",
-                    coordinates: location
-                }
-            }
+  try {
+    if (!req.body.location) throw "Location";
+    if (!req.body.distance) throw "Distance";
+    if (!req.body.categories) throw "Categories";
+  } catch (err) {
+    return error.invalidInput(req, res, `${err} field missing!`);
+  }
+  const { location, distance, categories } = req.body;
+  let query = {
+    location: {
+      $near: {
+        $maxDistance: distance,
+        $geometry: {
+          type: "Point",
+          coordinates: location
         }
+      }
     }
-    if (categories.length) query.filters = {$in: categories}
-    db.Pin.find(query).populate('filters').populate('details').exec()
-    .then(foundData => res.status(200).json({
-        status: 'success',
+  };
+  if (categories.length) query.filters = { $in: categories };
+  db.Pin.find(query)
+    .populate("filters")
+    .populate("details")
+    .exec()
+    .then(foundData =>
+      res.status(200).json({
+        status: "success",
         message: null,
         data: foundData.map(found => ({
-            id: found._id,
-            name: found.name,
-            address: found.address,
-            location: found.location.coordinates,
-            categories: found.filters.map(filter => ({id: filter._id, name: filter.name}))
+          id: found._id,
+          name: found.name,
+          address: found.address,
+          location: found.location.coordinates,
+          categories: found.filters.map(filter => ({
+            id: filter._id,
+            name: filter.name
+          }))
         }))
-    }))
-    .catch(err => error.serverError(req, res, err))
-}
+      })
+    )
+    .catch(err => error.serverError(req, res, err));
+};
 
 exports.createPin = (req, res) => {
-    const newPinDetails = {
-        description: req.body.details.description,
-        images: req.body.images,
-        menu: req.body.details.menu,
-        hours: req.body.details.hours,
-        comments: []
-    }
-    db.Detail.create(newPinDetails)
+  try {
+    if (!req.body.name) throw "Name";
+    if (!req.body.address) throw "Address";
+    if (!req.body.address.street) throw "Address-Street";
+    if (!req.body.address.number) throw "Address-Number";
+    if (!req.body.location) throw "Location";
+    if (!req.body.categories) throw "Categories";
+    if (!req.body.details) throw "Details";
+    if (!req.body.details.description) throw "Details-Description";
+    if (!req.body.details.images) throw "Details-Images";
+    if (!req.body.details.menu) throw "Details-Menu";
+    if (!req.body.details.hours) throw "Details-Hours";
+    if (!req.body.details.hours.monday) throw "Details-Hours-Monday";
+    if (!req.body.details.hours.monday.from) throw "Details-Hours-Monday-From";
+    if (!req.body.details.hours.monday.to) throw "Details-Hours-Monday-To";
+    if (!req.body.details.hours.tuesday) throw "Details-Hours-Tuesday";
+    if (!req.body.details.hours.tuesday.from)
+      throw "Details-Hours-Tuesday-From";
+    if (!req.body.details.hours.tuesday.to) throw "Details-Hours-Tuesday-To";
+    if (!req.body.details.hours.wednesday) throw "Details-Hours-Wednesday";
+    if (!req.body.details.hours.wednesday.from)
+      throw "Details-Hours-Wednesday-From";
+    if (!req.body.details.hours.wednesday.to)
+      throw "Details-Hours-Wednesday-To";
+    if (!req.body.details.hours.thursday) throw "Details-Hours-Thursday";
+    if (!req.body.details.hours.thursday.from)
+      throw "Details-Hours-Thursday-From";
+    if (!req.body.details.hours.thursday.to) throw "Details-Hours-Thursday-To";
+    if (!req.body.details.hours.friday) throw "Details-Hours-Friday";
+    if (!req.body.details.hours.friday.from) throw "Details-Hours-Friday-From";
+    if (!req.body.details.hours.friday.to) throw "Details-Hours-Friday-To";
+    if (!req.body.details.hours.saturday) throw "Details-Hours-Saturday";
+    if (!req.body.details.hours.saturday.from)
+      throw "Details-Hours-Saturday-From";
+    if (!req.body.details.hours.saturday.to) throw "Details-Hours-Saturday-To";
+    if (!req.body.details.hours.sunday) throw "Details-Hours-Sunday";
+    if (!req.body.details.hours.sunday.from) throw "Details-Hours-Sunday-From";
+    if (!req.body.details.hours.sunday.to) throw "Details-Hours-Sunday-To";
+  } catch (err) {
+    return error.invalidInput(req, res, `${err} field missing!`);
+  }
+  const newPinDetails = {
+    description: req.body.details.description,
+    images: req.body.images,
+    menu: req.body.details.menu,
+    hours: req.body.details.hours,
+    comments: []
+  };
+  db.Detail.create(newPinDetails)
     .then(createdDetail => {
-        const newPin = {
-            name: req.body.name,
-            address: req.body.address,
-            location: {
-                coordinates: req.body.location
-            },
-            filters: req.body.categories,
-            details: createdDetail._id
-        }
-        db.Pin.create(newPin)
+      const newPin = {
+        name: req.body.name,
+        address: req.body.address,
+        location: {
+          coordinates: req.body.location
+        },
+        filters: req.body.categories,
+        details: createdDetail._id
+      };
+      db.Pin.create(newPin)
         .then(createdPin => {
-            res.status(200).json({
-                status: 'success',
-                message: 'Created',
-                data: {
-                    id: createdPin._id,
-                    images: createdPin.images,
-                    address: createdPin.address,
-                    location: createdPin.location.coordinates,
-                    categories: createdPin.filters.map(obj => obj._id),
-                    details: {
-                        description: createdDetail.description,
-                        images: createdDetail.images,
-                        menu: createdDetail.menu,
-                        hours: createdDetail.hours,
-                        comments: createdDetail.comments
-                    }
-                }
-            })
+          res.status(200).json({
+            status: "success",
+            message: "Created",
+            data: {
+              id: createdPin._id,
+              name: createdPin.name,
+              images: createdPin.images,
+              address: createdPin.address,
+              location: createdPin.location.coordinates,
+              categories: createdPin.filters.map(obj => obj._id),
+              details: {
+                description: createdDetail.description,
+                images: createdDetail.images,
+                menu: createdDetail.menu,
+                hours: createdDetail.hours,
+                comments: createdDetail.comments
+              }
+            }
+          });
         })
-        .catch(err => error.serverError(req, res, err))
+        .catch(err => error.serverError(req, res, err));
     })
-    .catch(err => error.serverError(req, res, err))
-}
+    .catch(err => error.serverError(req, res, err));
+};
 
 exports.updatePin = (req, res) => {
-    // Change required ===
-    const id = { _id: req.body.id }
-    let change = {}
-    for (let val in req.body) change[val] = req.body[val]
-    if(change.location) change.location = {coordinates: change.location}
-    if(change.filters) {change.categories = change.filters; change.filters = undefined}
-    if(change.id) {change.id = undefined}
-    // ===
+  try {
+    if (!req.body.id) throw "Id";
+  } catch (err) {
+    return error.invalidInput(req, res, `${err} field missing!`);
+  }
+  const id = { _id: req.body.id };
+  let change = {};
+  ["name", "address", "location", "categories"].forEach(val => {
+    if (req.body[val]) change[val] = req.body[val];
+  });
+  if (change.location) change.location = { coordinates: change.location };
+  if (change.filters) {
+    change.categories = change.filters;
+    change.filters = undefined;
+  }
 
-    db.Pin.updateOne(id, change)
+  db.Pin.updateOne(id, change)
     .then(() => success.updated(req, res))
-    .catch(err => error.serverError(req, res, err))
-}
+    .catch(err => error.serverError(req, res, err));
+};
 
 exports.deletePin = (req, res) => {
-    error.notImplemented(req, res)
-}
+  try {
+    if (!req.body.id) throw "Id";
+  } catch (err) {
+    return error.invalidInput(req, res, `${err} field missing!`);
+  }
+  const id = { _id: req.body.id };
+  db.Pin.find(id)
+    .then(foundPin => {
+      db.Detail.deleteOne({ _id: foundPin[0].details })
+        .then(() => db.Pin.deleteOne(id))
+        .then(() => success.deleted(req, res))
+        .catch(err => error.serverError(req, res, err));
+    })
+    .catch(err => error.serverError(req, res, err));
+};
